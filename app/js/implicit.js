6c565bb46c51ea694af5ce536461baba193c4f34
Erik Ferlanti <eferlanti@tacc.utexas.edu>
2017-05-22 14:32:36 -0500

    Update implicit.js

--- app/js/implicit.js
+++ app/js/implicit.js
@@ -2,7 +2,7 @@
 var OAuthClients = {
     "agave.prod": {
         "clientKey": "_2xmhvwpwGOLwLqP6AkR9stGHoQa",
-        "callbackUrl": "http://localhost:8080/app/",
+        "callbackUrl": "https://eriksf.github.io/at_microsite/app/",
         "scope": "PRODUCTION"
     }
 };
